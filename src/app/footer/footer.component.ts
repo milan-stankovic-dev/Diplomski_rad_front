6f29b6828488a6c94d61acb692ef36a98c965b92
milan-stankovic-dev <milance.za019@gmail.com>
2023-07-01 11:13:07 +0200

    Added a header and a footer component to website. App component has header and footer in it now.

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `
+    <div class="container content has-text-centered">
+      <p>Bottom text</p>
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class FooterComponent {
+
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <div class = 'navbar is-dark'>
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          MY LOGO GOES HERE
+        </a>
+      </div>
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class HeaderComponent {
+
+}
